1814d52aeede1b2122d6edc73db83e08e19b37cd
Guilherme Simoes <guilherme-elec12@hotmail.com>
2023-03-03 20:32:23 -0300

    <feat> add button to see student info

--- scripts/student-contract.js
+++ scripts/student-contract.js
@@ -1,21 +1,21 @@
-function getStudentContracts () {
-    fetch('https://student-book.glitch.me/student/1/contracts')
+function getStudentsContracts () {
+    fetch('https://student-book.glitch.me/students/contracts')
       .then(response => response.json())
       .then(data => {
-        generateStudentContractTable(data);
         console.log(data);
+        generateStudentsContractTable(data);
       })
       .catch(error => console.error(error));
 }
 
-function generateStudentContractTable(data) {
-  for(let i = 0; i<data.studentContracts.length; i++) {
+function generateStudentsContractTable(data) {
+  for(let i = 0; i<data.studentsContracts.length; i++) {
     let row = $("<tr>");
-    let columnStudent = $("<td>").text(data.studentContracts[i].name);
-    let columnStartDate = $("<td>").text(data.studentContracts[i].start_date);
-    let columnEndDate = $("<td>").text(data.studentContracts[i].end_date);
-    let columnTime = $("<td>").text(data.studentContracts[i].hours_bought - data.studentContracts[i].hours_used);
-    let columnStatus = $("<td>").text(data.studentContracts[i].is_active? "active":"inactive");
+    let columnStudent = $("<td>").text(data.studentsContracts[i].name);
+    let columnStartDate = $("<td>").text(data.studentsContracts[i].start_date);
+    let columnEndDate = $("<td>").text(data.studentsContracts[i].end_date);
+    let columnTime = $("<td>").text(data.studentsContracts[i].hours_bought - data.studentsContracts[i].hours_used);
+    let columnStatus = $("<td>").text(data.studentsContracts[i].is_active? "active":"inactive");
 
     row.append(columnStudent);
     row.append(columnStartDate);
@@ -24,13 +24,19 @@ function generateStudentContractTable(data) {
     row.append(columnStatus);
 
     // instantiate button to register class for that particular student
-
-    let registerClassBtn = $("<button>");
-    registerClassBtn.text("Register class");
+    let registerClassBtn = $("<button>").text("Register class");
+    if(columnStatus.text() != "active") {
+      registerClassBtn.addClass("inactive");
+    }
     row.append($("<td>").append(registerClassBtn));
+
+    // instantiate button to see/change student data
+    let seeStudentBtn = $("<button>").text("See student");
+    row.append($("<td>").append(seeStudentBtn));
 
     $("#student-contract-table").find("tbody").append(row);
   }    
 }
 
-getStudentContracts();
+getStudentsContracts();
+//generateStudentContractTable({studentsContracts:[]});
